378be556387216f1b83f0441e823ef2576ecdc74
sunflowercactus <sobikfox@gmail.com>
2024-05-16 01:58:50 +0300

    cool thing

---
+++ demo/main.cpp
@@ -0,0 +1,14 @@
+include "../State.cpp"
+int main()
+{
+  Context *context = new Context();
+  
+  context->setState( new ConcreteStateA() );
+  context->request();
+  
+  context->setState( new ConcreteStateB() );
+  context->request();
+  
+  delete context;
+  return 0;
+}

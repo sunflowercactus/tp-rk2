1e0737803950d75904595aa51501518304252be2
soboleva_dasha <115922130+sunflowercactus@users.noreply.github.com>
2024-05-20 15:45:52 +0300

    ok???

--- State.cpp
+++ State.cpp
@@ -87,16 +87,3 @@ private:
 };
 
 
-int main()
-{
-  Context *context = new Context();
-  
-  context->setState( new ConcreteStateA() );
-  context->request();
-  
-  context->setState( new ConcreteStateB() );
-  context->request();
-  
-  delete context;
-  return 0;
-}
